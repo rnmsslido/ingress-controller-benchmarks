8f6a24fabb3fdcd62989812bc84572aaa1b6ff4c
Rami <rami@lido.fi>
2025-04-17 00:37:26 -0300

    Revert "More fixes"
    This reverts commit f1c96caf01ce3d08f9a8af330d4a03833e001b01.

--- tools/chart.py
+++ tools/chart.py
@@ -15,75 +15,80 @@
 
 import argparse
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Set style
-plt.style.use('ggplot')
-plt.rcParams['axes.facecolor']='white'
-plt.rcParams['savefig.facecolor']='white'
+#plt.style.use('ggplot')
+#plt.rcParams['axes.facecolor']='white'
+#plt.rcParams['savefig.facecolor']='white'
+
+proxies = ['Envoy', 'HAProxy', 'NGINX Inc.', 'NGINX', 'Traefik']
+
 
 def generate_single_bar(output, reqdata, title, ylabel, add_label=False):
-    # Create figure and axis
+    global proxies
+
     fig, ax = plt.subplots()
-    
-    # Prepare data
-    x_pos = np.arange(len(reqdata))
+    x_pos = [i for i, _ in enumerate(proxies)]
     rects = plt.bar(x_pos, reqdata, color=['royalblue'])
-    
-    # Set labels and title
     plt.xlabel("Proxy")
     plt.ylabel(ylabel)
     plt.title(title)
-    plt.xticks(x_pos, ['NGINX'])  # Only show NGINX for single controller
-    
-    # Add labels if requested
+    plt.xticks(x_pos, proxies)
     if add_label:
         autolabel(ax, rects)
-    
-    # Save figure
     plt.savefig(output)
-    plt.close()
+#    plt.show()
 
 def generate_grouped_bar(output, title, ylabel, bar1, bar2, bar3, add_label=False, percentiles=False):
-    # Set labels based on type
-    if percentiles:
-        label1 = '75th'
-        label2 = '95th'
-        label3 = '99th'
-    else:
+    global proxies
+    label1 = '75th'
+    label2 = '95th'
+    label3 = '99th'
+
+    if not percentiles:
         label1 = '502'
         label2 = '503'
         label3 = '504'
-    
-    # Create figure and axis
-    x = np.arange(1)  # Only one bar group for single controller
-    width = 0.2
+    x = np.arange(len(proxies)) 
+    width = 0.15 
     fig, ax = plt.subplots()
-    
-    # Create bars
     rects1 = ax.bar(x, bar1, width, label=label1, color="royalblue")
-    rects2 = ax.bar(x + width, bar2, width, label=label2, color="red")
-    rects3 = ax.bar(x + (2 * width), bar3, width, label=label3, color="orange")
-    
-    # Set labels and title
+    rects2 = ax.bar(x+width , bar2, width, label=label2, color="red")
+    rects3 = ax.bar(x+(2 * width), bar3, width, label=label3, color="orange")
     ax.set_ylabel(ylabel)
     ax.set_title(title)
-    ax.set_xticks(x + width)
-    ax.set_xticklabels(['NGINX'])
+    ax.set_xticks(x)
+    ax.set_xticklabels(proxies)
     ax.legend()
-    
-    # Add labels if requested
     if add_label:
-        autolabel(ax, rects1)
+        autolabel(ax, rects1) 
         autolabel(ax, rects2)
         autolabel(ax, rects3)
-    
-    # Adjust layout and save
     fig.tight_layout()
     plt.savefig(output)
-    plt.close()
+    #plt.show()
+
+def generate2_grouped_bar(status502, status503, status504):
+    global proxies
+    x = np.arange(len(proxies))
+    width = 0.15
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x, status502, width, label='502', color="royalblue")
+    rects2 = ax.bar(x+width , status503, width, label='503', color="red")
+    rects3 = ax.bar(x+(2 * width), status504, width, label='504', color="orange")
+    ax.set_ylabel('Count')
+    ax.set_title('Status code count')
+    ax.set_xticks(x)
+    ax.set_xticklabels(proxies)
+    ax.legend()
+    autolabel(ax, rects1)
+    autolabel(ax, rects2)
+    autolabel(ax, rects3)
+    fig.tight_layout()
+    plt.savefig(output)
+    #plt.show()
 
 def autolabel(ax, rects):
     """Attach a text label above each bar in *rects*, displaying its height."""
@@ -92,15 +97,16 @@ def autolabel(ax, rects):
         if int(float('{}'.format(height))) > 0:
             ax.annotate('{}'.format(height),
                     xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),
+                    xytext=(0, 3),  # 3 points vertical offset
                     textcoords="offset points",
                     ha='center', va='bottom')
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--chart", help="Type of chart to output", type=str, choices=["single","grouped"], required=True)
     parser.add_argument("-l", "--label", help="Print bar numbers", action="store_true")
-    parser.add_argument("-o", "--output", help="Full path to file to output", type=str, required=True)
+    parser.add_argument("-o", "--output", help="Full path to file to output", type=str, required=True) 
     parser.add_argument("-p", "--percentile", help="Use Percentile labels", action="store_true")
     parser.add_argument("-s", "--single-data", help="Data to chart", nargs='+', type=float, required=False)
     parser.add_argument("-t", "--title", help="Title of chart", type=str, required=True)
@@ -116,4 +122,4 @@ def main():
         generate_grouped_bar(args.output, args.title, args.ylabel, args.bar1, args.bar2, args.bar3, args.label, args.percentile)
 
 if __name__ == "__main__":
-    main()
+   main()
